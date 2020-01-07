0030136c6a4206d2f74b2551a27d2c647da472cd
Rifat Hossain Khan <rifat.hk889@gmail.com>
2020-01-07 18:34:31 +0600

    reducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './App.css';
-import CompnentC from './components/CompnentC';
+// import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+// import CompnentC from './components/CompnentC';
 // import DataFetching from './components/DataFetching';
 
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
+// export const UserContext = React.createContext();
+// export const ChannelContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      {/* <DataFetching /> */}
+      {/* <DataFetching />
       <UserContext.Provider value={'Rifat'}>
         <ChannelContext.Provider value={'V2 Technology'}>
           <CompnentC />
         </ChannelContext.Provider>
-      </UserContext.Provider>
+      </UserContext.Provider> */}
+      {/* <CounterOne /> */}
+      <CounterTwo/>
     </div>
   );
 }
